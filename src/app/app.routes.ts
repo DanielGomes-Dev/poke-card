387991b2829b0cfe7c456a7122560d2f2a1c1ed1
Daniel Gomes <danielgomes1@live.com>
2024-05-16 19:55:18 -0300

    wip: componente list criado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './error-routing/not-found/not-found.component';
 import { UncaughtErrorComponent } from './error-routing/error/uncaught-error.component';
+import { ListComponent } from './components/list/list.component';
 
 export const routes: Routes = [
-	{ path: '', redirectTo: '/home', pathMatch: 'full'},
-	{ path: 'home', component: HomeComponent, data: { text: 'Home' }},
-	{ path: 'error', component: UncaughtErrorComponent },
-	{ path: '**', component: PageNotFoundComponent } // must always be last
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, data: { text: 'Home' } },
+  { path: 'error', component: UncaughtErrorComponent },
+  { path: 'list', component: ListComponent, data: { text: 'list' } },
+  { path: '**', component: PageNotFoundComponent } // must always be last
 ];
