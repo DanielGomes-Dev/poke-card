5d68a0713051a6ba8da589c5b818f17a181d8b5f
Daniel Gomes <danielgomes1@live.com>
2024-05-19 13:20:26 -0300

    feat: adicionando cartas ao baralho

--- src/app/pages/deck-details/deck-details.component.ts
+++ src/app/pages/deck-details/deck-details.component.ts
@@ -5,21 +5,22 @@ import { ListComponent } from '../../components/list/list.component';
 import { CardService } from '../../services/card/card.service';
 import { Card } from 'pokemon-tcg-sdk-typescript/dist/sdk';
 import { CardViewComponent } from "../../components/card-view/card-view.component";
+import { ToastComponent } from "../../components/toast/toast.component";
 
 @Component({
     selector: 'app-deck-details',
     standalone: true,
     templateUrl: './deck-details.component.html',
     styleUrl: './deck-details.component.scss',
-    imports: [ListComponent, CardViewComponent]
+    imports: [ListComponent, CardViewComponent, ToastComponent]
 })
 export class DeckDetailsComponent implements OnInit {
-
+  toast = false;
   cardsInDeck: any = []
   cardsOutDeck: Card[] = []
 
   addCard = false;
-
+  deckId: string = ""
   constructor(private route: ActivatedRoute, private cardInDeckService: CardInDeckService, private cardService: CardService,
   ) {
     
@@ -28,8 +29,8 @@ export class DeckDetailsComponent implements OnInit {
   async ngOnInit() {
     this.route.queryParams.subscribe(params => {
       console.log(params['deckId']);
-      const deckId = params['deckId'];
-      this.getAllCardInDeck(deckId)
+      this.deckId = params['deckId'];
+      this.getAllCardInDeck(this.deckId)
     });
   }
 
@@ -41,8 +42,15 @@ export class DeckDetailsComponent implements OnInit {
 
   async showCardsToAdd(){
     this.addCard = true;
-    this.cardsOutDeck = await this.cardService.getAll()
-    console.log(this.cardsOutDeck);
+    const allCards = await this.cardService.getAll();
+    this.cardsInDeck.forEach((card:any) => {
+      const response = allCards.findIndex((ac:any) => ac.id == card.cardId)
+      if(response != -1){
+        allCards.splice(response, 1);
+      }
+    })
+
+    this.cardsOutDeck = this.cardsOutDeck.concat(allCards);
   }
 
  
@@ -83,16 +91,29 @@ export class DeckDetailsComponent implements OnInit {
     console.log(this.cardsOutDeck,'cardsOutDeck');
   }
 
+  showToast(){
+    this.toast = true;
+    setTimeout(()=>{
+      this.toast = false;
+    },2000)
+  }
 
-  saveAllCardsAddInDeck(){
+
+  async saveAllCardsAddInDeck(){
     const cardsToSave = []
     for (const card of this.cardsInDeck) {
-      if(card.new){
+      if(card.isNew){
+        delete card.isNew
         cardsToSave.push(card);
       }
     }
-
-    console.log(cardsToSave)
+    //refatorar
+    for (const card of cardsToSave) {
+      await this.cardInDeckService.insertCardsInDeck(this.deckId, card)
+    }
+    console.log(cardsToSave,'cardsToSave')
+    this.showToast();
+    this.getAllCardInDeck(this.deckId)
 
   }
 
